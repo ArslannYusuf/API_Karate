3361c2079f4e3889e0c21e1bf19a75bf56dd2a3f
yusuf <yeed060118@gmail.com>
2024-01-01 12:33:04 +0300

    karate framework

---
+++ src/test/java/jsonPlaceHolder/JphRunner.java
@@ -0,0 +1,16 @@
+package jsonPlaceHolder;
+
+
+import com.intuit.karate.junit4.Karate;
+import cucumber.api.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Karate.class)
+@CucumberOptions(
+        features = "",
+        tags = "@api"
+)
+
+public class JphRunner {
+
+}
